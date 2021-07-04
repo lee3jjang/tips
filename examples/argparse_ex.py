c8881d89f02c7bb8e8ac7d7a4c7d354ef401451a
Sang-Jin <lee3jjang@gmail.com>
2021-07-04 12:50:19 +0900

    2211

--- examples/argparse_ex.py
+++ examples/argparse_ex.py
@@ -6,4 +6,6 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument('-s', '--src', nargs='?', help='src help', required=False, default=argparse.SUPPRESS)
+parser.add_argument('-n', '--num_workers', nargs='?', type=int, default=4, required=False)
+parser.add_argument('bar', nargs='+', type=int, help='bar help')
 args = parser.parse_args()
